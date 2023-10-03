6c06fbcb213447af4d676eed11911029ff77da72
Caleb Huang <calebhuangsea@gmail.com>
2023-10-02 19:36:37 -0700

    initialize backend

---
+++ x-admin/src/main/java/com/caleb/common/vo/Result.java
@@ -0,0 +1,15 @@
+package com.caleb.common.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result <T> {
+    private Integer code;
+    private String msg;
+    private T data;
+
+}
